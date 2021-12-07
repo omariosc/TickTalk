4bf2a5c8dd9a28faa63dcfcb8d11bb8bbf4d3ab1
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-07 20:23:41 +0000

    If user is logged in, redirects to home page

--- app/index.py
+++ app/index.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, redirect
+from flask_login import current_user
 
 index = Blueprint('index', __name__, template_folder='/templates')
 
 @index.route('/', methods=['GET'])
 def show():
+  if current_user.is_authenticated:
+    return redirect('home') 
   return redirect('login')

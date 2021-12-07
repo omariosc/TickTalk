c1adbfb9b9471b1d49deb335fe4e27d8eee4c4f0
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-07 02:01:35 +0000

    Updated database Created room and messages models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,16 @@ db = SQLAlchemy()
 
 class Users(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(15), unique=True)
+  username = db.Column(db.String(20), unique=True)
   email = db.Column(db.String(50), unique=True)
   password = db.Column(db.String)
+  
+class Room(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  count = db.Column(db.Integer)
+
+class Messages(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  room = db.Column(db.Integer)
+  user = db.Column(db.Integer)
+  message = db.Column(db.String)

04267f3f12da6957d3fdfb8ba100ed2d31add7b0
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-07 00:31:03 +0000

    Changed cookie warning colour and position

--- app/static/js/cookie.js
+++ app/static/js/cookie.js
@@ -1,7 +1,7 @@
 window.cookieconsent.initialise({
   "palette": {
     "popup": {
-      "background": "#0069d9",
+      "background": "#343A40",
       "text": "#ffffff"
     },
     "button": {
@@ -9,7 +9,7 @@ window.cookieconsent.initialise({
       "text": "#000000"
     }
   },
-  "position": "bottom-right",
+  "position": "bottom-left",
   "content": {
     "message": "This website uses cookies to ensure you get the best experience on our website.",
     "dismiss": "Got it!",

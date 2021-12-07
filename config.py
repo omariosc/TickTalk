ac91595678a20d9e2c2fc06eaf4f68efd463ea86
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-07 20:23:26 +0000

    Renamed database

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'sc20osc'
 
 # Sets path and configuration for database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///../users.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///../app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True

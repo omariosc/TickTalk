3b436b6e2ede24460c6a7fb39bcb6b10688ff2d0
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-08 00:33:00 +0000

    Created many to many relationship for user and rooms Added database relationships

--- app/models.py
+++ app/models.py
@@ -8,13 +8,21 @@ class Users(UserMixin, db.Model):
   username = db.Column(db.String(20), unique=True)
   email = db.Column(db.String(50), unique=True)
   password = db.Column(db.String)
-  
-class Room(db.Model):
+
+class Rooms(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  count = db.Column(db.Integer)
+
+class UsersRooms(db.Model):
+  user_id = db.Column(db.Integer, db.ForeignKey(Users.id), primary_key=True)
+  room_id = db.Column(db.Integer, db.ForeignKey(Rooms.id), primary_key=True)  
+  user = db.relationship('User', foreign_keys='Users.id')
+  room = db.relationship('Room', foreign_keys='Rooms.id')
 
 class Messages(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  room = db.Column(db.Integer)
-  user = db.Column(db.Integer)
+  user_id = db.Column(db.Integer, db.ForeignKey(Users.id))
+  room_id = db.Column(db.Integer, db.ForeignKey(Rooms.id))
   message = db.Column(db.String)
+  time = db.Column(db.Date)
+  user = db.relationship('User', foreign_keys='Users.id')
+  room = db.relationship('Room', foreign_keys='Rooms.id')

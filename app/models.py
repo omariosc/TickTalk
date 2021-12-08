f4f4fd0c5a077d9c16bc6404ea8cedcdbfd5a2f2
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-08 21:06:58 +0000

    Updated database Added all relationships and foreign keys

--- app/models.py
+++ app/models.py
@@ -1,28 +1,32 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import backref
 
 db = SQLAlchemy()
 
+class UserRooms(db.Model):
+  __tablename__ = 'UserRooms'
+  id = db.Column(db.Integer, primary_key=True)
+  user = db.Column(db.ForeignKey('Users.id'))
+  room = db.Column(db.ForeignKey('Rooms.id'))
+  message = db.relationship("UserRoom", secondary='Messages')
+
 class Users(UserMixin, db.Model):
+  __tablename__ = 'Users'
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(20), unique=True)
   email = db.Column(db.String(50), unique=True)
   password = db.Column(db.String)
+  room = db.relationship("User", secondary='UserRooms')
 
 class Rooms(db.Model):
+  __tablename__ = 'Rooms'
   id = db.Column(db.Integer, primary_key=True)
-
-class UsersRooms(db.Model):
-  user_id = db.Column(db.Integer, db.ForeignKey(Users.id), primary_key=True)
-  room_id = db.Column(db.Integer, db.ForeignKey(Rooms.id), primary_key=True)  
-  user = db.relationship('User', foreign_keys='Users.id')
-  room = db.relationship('Room', foreign_keys='Rooms.id')
-
+  user = db.relationship("Room", secondary='UserRooms')
+  
 class Messages(db.Model):
+  __tablename__ = 'Messages'
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey(Users.id))
-  room_id = db.Column(db.Integer, db.ForeignKey(Rooms.id))
+  userroom = db.Column(db.ForeignKey('UserRooms.id'))
   message = db.Column(db.String)
   time = db.Column(db.Date)
-  user = db.relationship('User', foreign_keys='Users.id')
-  room = db.relationship('Room', foreign_keys='Rooms.id')

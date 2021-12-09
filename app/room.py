accd9febcce5765360a613d3192554b704148b3e
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-09 03:40:38 +0000

    Error checking for accessing rooms Renders chatroom template with input box and submit chat button Gets all messages in room Added room styling sheet for form and container Renamed settings styling sheet classes Added room styling sheet to template and form Removed create_errors template

--- app/room.py
+++ app/room.py
@@ -1,14 +1,25 @@
 from flask import Blueprint, url_for, redirect
 from flask.templating import render_template
 from flask_login import login_required, current_user
-from app.models import db, Rooms, UserRooms
+from app.models import db, Rooms, UserRooms, Messages
 
 room = Blueprint('room', __name__, template_folder='/templates')
 
 @room.route('/room/<room_id>', methods=['GET'])
 @login_required
 def show(room_id):
-  return render_template('chatroom.html')
+  if len(Rooms.query.filter_by(id=room_id).all()) == 0:
+    return redirect(url_for('home.show') + '?error=room-not-exists')
+  if UserRooms.query.filter_by(user=current_user.id, room=room_id).count() == 0:
+    return redirect(url_for('home.show') + '?error=must-join-room')
+  else:
+    userrooms = UserRooms.query.filter_by(room=room_id).all()
+    messages = []
+    for i in range(len(userrooms)):
+      userroom_messages = Messages.query.filter_by(userroom_id=userrooms[i].id).all()
+      for message in userroom_messages:
+        messages.append(message)
+    return render_template('chatroom.html', messages=messages, userroom_id=UserRooms.query.filter_by(user=current_user.id, room=room_id).one().id)
 
 @room.route('/room/join/<room_id>', methods=['GET'])
 @login_required

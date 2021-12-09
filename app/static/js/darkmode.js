87eb1f1732a65a59a21421f8da5baf0f5a44d6ad
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-09 00:47:38 +0000

    Added styling Set max width and removed horizontal scrollbar Added custom darkmode color changes Added darkmode js file. Changes to size of button Replaced default darkmode options with custom ones Added style sheet to base.html Initial chatroom.html file Removed room links from navbar Created room.html to display rooms as decks, including total members and messages

--- app/static/js/darkmode.js
+++ app/static/js/darkmode.js
@@ -1,17 +1,4 @@
 function addDarkmodeWidget() {
-  const options = {
-    bottom: '16px',
-    right: '16px',
-    left: 'unset',
-    time: '0.5s',
-    mixColor: '#fff',
-    backgroundColor: '#fff',
-    buttonColorDark: '#100f2c',
-    buttonColorLight: '#fff',
-    saveInCookies: true,
-    label: '🌓',
-    autoMatchOsTheme: true
-  }
-  new Darkmode(options).showWidget();
+  new Darkmode().showWidget();
 }
 window.addEventListener('load', addDarkmodeWidget);

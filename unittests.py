e9eb94eef56ee3d2538b6be88b5dc0137ee45608
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-11 00:01:02 +0000

    Update unit tests file

--- unittests.py
+++ unittests.py
@@ -1,24 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app import app, db, models
-import os
-import unittest
+from app import app,login,logout,logs,models,register,room,settings
+import os, unittest
 
 class TestCase(unittest.TestCase):
   def setUp(self):
     app.config.from_object('config')
     app.config['TESTING'] = True
-    app.config['WTF_CSRF_ENABLED'] = False
-    #the basedir lines could be added like the original db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['WTF_CSRF_ENABLED'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     self.app = app.test_client()
-    db.create_all()
+    models.db.create_all()
     pass
 
   def tearDown(self):
-    db.session.remove()
-    db.drop_all()
+    models.db.session.remove()
+    models.db.drop_all()
     
   def test_addtaskroute(self):
     response = self.app.get('/add_task',follow_redirects=True)
     self.assertEqual(response.status_code, 200)
+  

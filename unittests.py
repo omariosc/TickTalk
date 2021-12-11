2c320edac204dac7e0bb865b092e439f82f24193
Omar Choudhry <71817542+omariosc@users.noreply.github.com>
2021-12-11 16:42:58 +0000

    Updated requirements and unit tests

--- unittests.py
+++ unittests.py
@@ -1,16 +1,65 @@
-from flask import Flask
+from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy
-from app import app,db,login,logout,logs,models,register,room,settings
+from flask_login import LoginManager,current_user
+from app.models import db,Users,Rooms,UserRooms,Messages,Logs
+from app.index import index
+from app.login import login
+from app.logout import logout
+from app.register import register
+from app.home import home
+from app.settings import settings
+from app.room import room
+from app.myrooms import myrooms
+from app.logs import log_error
 import unittest
 
 class TestCase(unittest.TestCase):
   def setUp(self):
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    self.app = app.test_client()
+    self.app = Flask(__name__, template_folder='app/templates')
+    self.app.config['TESTING'] = True
+    self.app.config['WTF_CSRF_ENABLED'] = False
+    self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    login_manager=LoginManager()
+    login_manager.init_app(self.app)
+    db.init_app(self.app)
+    self.app.app_context().push()
+    self.app.register_blueprint(index)
+    self.app.register_blueprint(login)
+    self.app.register_blueprint(logout)
+    self.app.register_blueprint(register)
+    self.app.register_blueprint(home)
+    self.app.register_blueprint(settings)
+    self.app.register_blueprint(room)
+    self.app.register_blueprint(myrooms)
+    self.app = self.app.test_client()
+    @login_manager.user_loader
+    def load_user(id):
+      return Users.query.get(int(id))
     pass
 
   def tearDown(self):
+    self.app = Flask(__name__, template_folder='app/templates')
+    self.app.config['TESTING'] = True
+    self.app.config['WTF_CSRF_ENABLED'] = False
+    self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    login_manager=LoginManager()
+    login_manager.init_app(self.app)
+    db.init_app(self.app)
+    self.app.app_context().push()
+    self.app.register_blueprint(index)
+    self.app.register_blueprint(login)
+    self.app.register_blueprint(logout)
+    self.app.register_blueprint(register)
+    self.app.register_blueprint(home)
+    self.app.register_blueprint(settings)
+    self.app.register_blueprint(room)
+    self.app.register_blueprint(myrooms)
+    self.app = self.app.test_client()
+    @login_manager.user_loader
+    def load_user(id):
+      return Users.query.get(int(id))
     db.session.remove()
     db.drop_all()
     
@@ -25,6 +74,35 @@ class TestCase(unittest.TestCase):
   def test_load_index(self):
     response = self.app.get('/',follow_redirects=True)
     self.assertEqual(response.status_code, 200)
+    
+  def test_unauthorized_home(self):
+    response = self.app.get('/home',follow_redirects=True)
+    self.assertEqual(response.status_code, 401)
+    
+  def test_unauthorized_settings(self):
+    response = self.app.get('/settings',follow_redirects=True)
+    self.assertEqual(response.status_code, 401)
+    
+  def test_unauthorized_create_room(self):
+    response = self.app.get('/room/create',follow_redirects=True)
+    self.assertEqual(response.status_code, 401)
+    
+  def test_unauthorized_join_room(self):
+    response = self.app.get('/room/join/1',follow_redirects=True)
+    self.assertEqual(response.status_code, 401)
+    
+  def test_unauthorized_leave_room(self):
+    response = self.app.get('/room/leave/1',follow_redirects=True)
+    self.assertEqual(response.status_code, 401)
+    
+  def test_unauthorized_view_room(self):
+    response = self.app.get('/room/1',follow_redirects=True)
+    self.assertEqual(response.status_code, 401)
+    
+  def test_not_found(self):
+    response = self.app.get('/randomurl',follow_redirects=True)
+    self.assertEqual(response.status_code, 404)
+  
 
 if __name__ == '__main__':
   unittest.main()
